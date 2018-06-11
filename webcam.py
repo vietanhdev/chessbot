ace18107ceabe614341d2688868ed81e1330884b
Viet-Anh Nguyen <vietanh@vietanhdev.com>
2018-06-12 00:12:21 +0700

    add init code for piece recognition

--- webcam.py
+++ webcam.py
@@ -9,9 +9,10 @@ import neural_chessboard
 
 
 pathlib.Path('./tmp').mkdir(parents=True, exist_ok=True) 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.namedWindow('canny', cv2.WINDOW_NORMAL)
 
 while(True):
 
@@ -27,27 +28,63 @@ while(True):
     key = cv2.waitKey(10)
 
     if key == 13:
-        try:
+        # try:
            
             transformMatrices = board_detector.detect(frame)
             result = board_detector.getCropImage(frame, transformMatrices)
             
             cv2.imshow('image', result)
-            cv2.waitKey(0)
+            # cv2.waitKey(0)
 
             # Use tranformMatrices to crop 100 next images
-            for _ in range(100):
+            for _ in range(10000):
                 # Capture frame-by-frame
                 ret, frame = cap.read()
                 if not ret:
                     continue
 
-                result = board_detector.getCropImage(frame, transformMatrices)
-                cv2.imshow('image', result)
+                crop = board_detector.getCropImage(frame, transformMatrices)
+
+                # Unpack shape of crop
+                rows,cols,channels = crop.shape
+
+                # crop[:row/8,:cols/8] = (0,0,255)
+
+                square_size = int (rows / 8)
+
+                np.count_nonzero
+
+                # Canny
+                canny = cv2.Canny(crop,50,150)
+
+
+                # Detect if there is a piece
+                # Padding to the center of the square
+                borderPadding = 25
+
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                for i in range(8):
+                    for j in range(8):
+
+                        sq = canny[i*square_size + borderPadding:(i+1)*square_size - borderPadding * 2:, j*square_size + borderPadding:(j+1)*square_size - borderPadding * 2:]
+
+                        crop[i*square_size + borderPadding:(i+1)*square_size - borderPadding * 2:, j*square_size + borderPadding:(j+1)*square_size - borderPadding * 2:, ::] = (0,255,0)
+                        
+                        # Count non zero point
+                        numOfNonZero = cv2.countNonZero(sq)
+
+                        if (numOfNonZero > 60):
+                            crop[i*square_size + borderPadding:(i+1)*square_size - borderPadding * 2:, j*square_size + borderPadding:(j+1)*square_size - borderPadding * 2:, ::] = (0,0,255)
+
+
+                cv2.imshow('canny', canny)
+                cv2.waitKey(1)
+
+                cv2.imshow('image', crop)
                 cv2.waitKey(1)
             
-        except:
-            print("Error in detecting board")
+        # except:
+        #     print("Error in detecting board")
 
     elif key & 0xFF == ord('q'):
         break
